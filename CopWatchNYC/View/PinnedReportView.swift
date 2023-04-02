eb0ff7325661afb7474f740bd660de393b71392c
Steve Roy <56884185+steveroy126@users.noreply.github.com>
2023-04-02 14:31:13 -0400

    Finished

--- CopWatchNYC/View/PinnedReportView.swift
+++ CopWatchNYC/View/PinnedReportView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct PinnedReportView: View {
     @State private var comment: String = ""
-    @State private var likes: Int = 100
-    @State private var dislikes: Int = 0
+    @State private var views: Int = 100
     
     var reportTitle: String
     var location: String
@@ -20,36 +19,50 @@ struct PinnedReportView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             Text(reportTitle)
-                .font(.title)
+                .font(.custom("HelveticaNeue-Bold", size: 26))
+                   .foregroundColor(.white)
             
             Text(description)
                 .font(.subheadline)
+                .fontWeight(.light)
+                .foregroundColor(.white)
             
             HStack(alignment: .center) {
                 Image(systemName: "location.fill")
+                    .foregroundColor(.white)
                 
                 Text("Location: \(location)")
                     .font(.caption)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
             }
             
             HStack(alignment: .center) {
                 Image(systemName: "clock.fill")
+                    .foregroundColor(.white)
                 
                 Text("Time: \(time)")
                     .font(.caption)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
             }
             
             HStack(alignment: .center) {
                 Image(systemName: "eye.fill")
+                    .foregroundColor(.white)
                 
-                Text("Views: \(likes)")
+                Text("Views: \(views)")
                     .font(.caption)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
             }
             
             Divider()
+                .foregroundColor(.white)
             
             Text("Comments")
                 .font(.headline)
+                .foregroundColor(.white)
             
             ScrollView {
                 VStack(alignment: .leading, spacing: 16) {
@@ -60,6 +73,7 @@ struct PinnedReportView: View {
             }
             
             Divider()
+                .foregroundColor(.white)
             
             HStack {
                 TextField("Add a comment...", text: $comment)
@@ -68,14 +82,18 @@ struct PinnedReportView: View {
                 
                 Button(action: {}) {
                     Text("Post")
+                        .foregroundColor(.white)
                         .padding(.horizontal, 16)
                 }
                 .disabled(comment.isEmpty)
+                .background(comment.isEmpty ? Color.gray : Color.blue)
+                .cornerRadius(5)
                 .padding(.trailing, 8)
             }
             .padding(.bottom, 16)
         }
         .padding()
+        .background(Color("Color"))
     }
 }
 
@@ -84,40 +102,52 @@ struct CommentView: View {
     @State private var dislikes: Int = 0
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text("Username")
-                .font(.headline)
+        HStack(alignment: .top, spacing: 8) {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.white)
             
-            Text("This is a comment.")
-                .font(.body)
-            
-            HStack {
-                Button(action: {
-                    likes += 1
-                }) {
-                    Image(systemName: "hand.thumbsup.fill")
-                    
-                    Text("\(likes)")
-                }
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Username")
+                    .font(.headline)
+                    .foregroundColor(.white)
                 
-                Button(action: {
-                    dislikes += 1
-                }) {
-                    Image(systemName: "hand.thumbsdown.fill")
+                Text("This is a comment for testing purposes.")
+                    .font(.body)
+                    .foregroundColor(.white)
+                
+                HStack {
+                    Button(action: {
+                        likes += 1
+                    }) {
+                        Image(systemName: "hand.thumbsup.fill")
+                        
+                        Text("\(likes)")
+                    }
+                    .foregroundColor(.blue)
                     
-                    Text("\(dislikes)")
+                    Button(action: {
+                        dislikes += 1
+                    }) {
+                        Image(systemName: "hand.thumbsdown.fill")
+                        
+                        Text("\(dislikes)")
+                    }
+                    .foregroundColor(.blue)
                 }
+                .font(.caption)
             }
-            .font(.caption)
         }
+        .padding(.vertical, 8)
     }
 }
 
 struct PinnedReportView_Previews: PreviewProvider {
     static var previews: some View {
-        PinnedReportView(reportTitle: "Cop Presence Nearby",
+        PinnedReportView(reportTitle: "Cop in Subway Station",
                          location: "695 Park Ave, New York, NY 10065",
                          time: "6:30 PM",
-                         description: "Police officers running in the Subway Station")
+                         description: "There are three police officers standing by the turnstiles")
     }
 }
